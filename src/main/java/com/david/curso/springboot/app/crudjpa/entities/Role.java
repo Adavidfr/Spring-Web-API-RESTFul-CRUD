283679ece1caefead9245c7c0d982caa3be93824
Alex David Frías <adavidfruiz@gmail.com>
2024-04-04 16:17:32 -0500

    add Role class

---
+++ src/main/java/com/david/curso/springboot/app/crudjpa/entities/Role.java
@@ -0,0 +1,38 @@
+package com.david.curso.springboot.app.crudjpa.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
