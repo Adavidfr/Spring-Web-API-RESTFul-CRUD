a206ec10a1b879ce9c0702acca1411e876d95170
Alex David Frías <adavidfruiz@gmail.com>
2024-04-24 02:02:39 -0500

    add UserController

---
+++ src/main/java/com/david/curso/springboot/app/crudjpa/controllers/UserController.java
@@ -0,0 +1,45 @@
+package com.david.curso.springboot.app.crudjpa.controllers;
+
+import com.david.curso.springboot.app.crudjpa.entities.User;
+import com.david.curso.springboot.app.crudjpa.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public List<User> list() {
+        return service.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@Valid @RequestBody User user, BindingResult result) {
+        if (result.hasFieldErrors()) {
+            return validation(result);
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(user));
+    }
+
+    private ResponseEntity<?> validation(BindingResult result) {
+        Map<String, String> errors = new HashMap<>();
+
+        result.getFieldErrors().forEach(err -> {
+            errors.put(err.getField(), "El campo " + err.getField() + " " + err.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+}
