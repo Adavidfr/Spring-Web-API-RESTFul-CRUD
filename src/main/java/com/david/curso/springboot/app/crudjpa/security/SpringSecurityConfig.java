107e2ee0798d5a84a621b9bb356b000cac2b8957
Alex David Frías <adavidfruiz@gmail.com>
2024-04-26 01:40:50 -0500

    add authorizations

--- src/main/java/com/david/curso/springboot/app/crudjpa/security/SpringSecurityConfig.java
+++ src/main/java/com/david/curso/springboot/app/crudjpa/security/SpringSecurityConfig.java
@@ -2,8 +2,12 @@ package com.david.curso.springboot.app.crudjpa.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -11,5 +15,16 @@ public class SpringSecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests( (authz) -> authz
+                .requestMatchers(HttpMethod.GET, "/api/users").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
+                .anyRequest().authenticated())
+                .csrf(config -> config.disable())
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 }
