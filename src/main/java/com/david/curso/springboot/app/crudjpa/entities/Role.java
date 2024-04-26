35edc77385f8e7b251e11102aad79a98ea728375
Alex David Frías <adavidfruiz@gmail.com>
2024-04-26 01:40:22 -0500

    add users list and annotations

--- src/main/java/com/david/curso/springboot/app/crudjpa/entities/Role.java
+++ src/main/java/com/david/curso/springboot/app/crudjpa/entities/Role.java
@@ -1,6 +1,11 @@
 package com.david.curso.springboot.app.crudjpa.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "roles")
@@ -13,7 +18,12 @@ public class Role {
     @Column(unique = true)
     private String name;
 
+    @JsonIgnoreProperties({"roles", "handler", "hibernateLazyInitializer"})
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
     public Role() {
+        this.users = new ArrayList<>();
     }
 
     public Role(String name) {
@@ -34,5 +44,26 @@ public class Role {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id) && Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
