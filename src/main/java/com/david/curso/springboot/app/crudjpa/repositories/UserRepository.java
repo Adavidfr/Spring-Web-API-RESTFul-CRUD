31e84fd1290389dc3944d5be2ce0ea8434d932f9
Alex David Frías <adavidfruiz@gmail.com>
2024-04-27 16:06:50 -0500

    add existsByUsername method

--- src/main/java/com/david/curso/springboot/app/crudjpa/repositories/UserRepository.java
+++ src/main/java/com/david/curso/springboot/app/crudjpa/repositories/UserRepository.java
@@ -3,5 +3,9 @@ package com.david.curso.springboot.app.crudjpa.repositories;
 import com.david.curso.springboot.app.crudjpa.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+    boolean existsByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
