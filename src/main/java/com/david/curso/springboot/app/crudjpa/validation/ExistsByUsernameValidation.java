f4a3612888e6926431597f569777a0e33cc65091
Alex David Frías <adavidfruiz@gmail.com>
2024-04-27 16:05:12 -0500

    add ExistsByUsernameValidation

---
+++ src/main/java/com/david/curso/springboot/app/crudjpa/validation/ExistsByUsernameValidation.java
@@ -0,0 +1,19 @@
+package com.david.curso.springboot.app.crudjpa.validation;
+
+import com.david.curso.springboot.app.crudjpa.services.UserService;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExistsByUsernameValidation implements ConstraintValidator<ExistsByUsername, String> {
+
+    @Autowired
+    private UserService service;
+
+    @Override
+    public boolean isValid(String username, ConstraintValidatorContext context) {
+        return !service.existsByUsername(username);
+    }
+}
