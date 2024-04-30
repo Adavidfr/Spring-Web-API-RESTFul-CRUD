508395384d015eb99c740196bcaeabd40449a149
Alex David Frías <adavidfruiz@gmail.com>
2024-04-30 01:34:48 -0500

    feat: filter

--- src/main/java/com/david/curso/springboot/app/crudjpa/security/SpringSecurityConfig.java
+++ src/main/java/com/david/curso/springboot/app/crudjpa/security/SpringSecurityConfig.java
@@ -1,8 +1,12 @@
 package com.david.curso.springboot.app.crudjpa.security;
 
+import com.david.curso.springboot.app.crudjpa.security.filter.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +15,14 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
+
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -23,6 +35,7 @@ public class SpringSecurityConfig {
                 .requestMatchers(HttpMethod.GET, "/api/users").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
                 .anyRequest().authenticated())
+                .addFilter((new JwtAuthenticationFilter(authenticationManager())))
                 .csrf(config -> config.disable())
                 .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
