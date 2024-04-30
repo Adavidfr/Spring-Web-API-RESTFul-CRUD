2658b54362efa9edcead38100893d259ecdaa0ca
Alex David Frías <adavidfruiz@gmail.com>
2024-04-30 01:33:54 -0500

    feat: add JwtValidationFilter

---
+++ src/main/java/com/david/curso/springboot/app/crudjpa/security/filter/JwtValidationFilter.java
@@ -0,0 +1,42 @@
+package com.david.curso.springboot.app.crudjpa.security.filter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import java.io.IOException;
+
+import static com.david.curso.springboot.app.crudjpa.security.TokenJwtConfig.*;
+
+public class JwtValidationFilter extends BasicAuthenticationFilter {
+
+    public JwtValidationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        String header = request.getHeader(HEADER_AUTHORIZATION);
+        if (header == null || !header.startsWith(PREFIX_TOKEN)) {
+            return;
+        }
+
+        String token = header.replace(PREFIX_TOKEN, "");
+
+        try {
+            Claims claims = Jwts.parser().verifyWith(SECRET_KEY).build().parseSignedClaims(token).getPayload();
+
+        }catch (JwtException e) {
+
+        }
+
+
+    }
+}
