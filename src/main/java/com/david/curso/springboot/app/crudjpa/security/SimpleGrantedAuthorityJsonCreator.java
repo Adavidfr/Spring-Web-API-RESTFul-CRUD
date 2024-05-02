302ce35eefa18acaf36310e603be951aa3d1e789
Alex David Frías <adavidfruiz@gmail.com>
2024-05-02 02:24:20 -0500

    feat: add SimpleGrantedAuthorityJsonCreator class

---
+++ src/main/java/com/david/curso/springboot/app/crudjpa/security/SimpleGrantedAuthorityJsonCreator.java
@@ -0,0 +1,11 @@
+package com.david.curso.springboot.app.crudjpa.security;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class SimpleGrantedAuthorityJsonCreator {
+
+    @JsonCreator
+    public SimpleGrantedAuthorityJsonCreator(@JsonProperty("authority") String role) {}
+
+}
