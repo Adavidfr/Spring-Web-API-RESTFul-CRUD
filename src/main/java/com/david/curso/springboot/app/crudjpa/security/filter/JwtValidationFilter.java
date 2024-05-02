7ad41eb6ae5a0c820bcacc58334c4d4c87cabd03
Alex David Frías <adavidfruiz@gmail.com>
2024-05-02 02:22:36 -0500

    feat: add method

--- src/main/java/com/david/curso/springboot/app/crudjpa/security/filter/JwtValidationFilter.java
+++ src/main/java/com/david/curso/springboot/app/crudjpa/security/filter/JwtValidationFilter.java
@@ -1,5 +1,7 @@
 package com.david.curso.springboot.app.crudjpa.security.filter;
 
+import com.david.curso.springboot.app.crudjpa.security.SimpleGrantedAuthorityJsonCreator;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
@@ -7,10 +9,19 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.david.curso.springboot.app.crudjpa.security.TokenJwtConfig.*;
 
@@ -25,6 +36,7 @@ public class JwtValidationFilter extends BasicAuthenticationFilter {
 
         String header = request.getHeader(HEADER_AUTHORIZATION);
         if (header == null || !header.startsWith(PREFIX_TOKEN)) {
+            chain.doFilter(request, response);
             return;
         }
 
@@ -32,9 +44,25 @@ public class JwtValidationFilter extends BasicAuthenticationFilter {
 
         try {
             Claims claims = Jwts.parser().verifyWith(SECRET_KEY).build().parseSignedClaims(token).getPayload();
+            String username = claims.getSubject();
+            Object authoritiesClaims = claims.get("authorities");
 
+            Collection<? extends GrantedAuthority> authorities = Arrays.asList(
+                    new ObjectMapper()
+                            .addMixIn(SimpleGrantedAuthority.class, SimpleGrantedAuthorityJsonCreator.class)
+                            .readValue(authoritiesClaims.toString().getBytes(), SimpleGrantedAuthority[].class));
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, null, authorities);
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            chain.doFilter(request, response);
         }catch (JwtException e) {
+            Map<String, String> body = new HashMap<>();
+            body.put("error", e.getMessage());
+            body.put("message", "El token JWT es invalido!");
 
+            response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType(CONTENT_TYPE);
         }
 
 
