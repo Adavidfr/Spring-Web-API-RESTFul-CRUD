d6dd5ca75aa334c95e2632cf1f8235c69b65deef
Alex David Frías <adavidfruiz@gmail.com>
2024-05-02 02:26:07 -0500

    feat: add CrossOrigin annotation

--- src/main/java/com/david/curso/springboot/app/crudjpa/controllers/UserController.java
+++ src/main/java/com/david/curso/springboot/app/crudjpa/controllers/UserController.java
@@ -13,6 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:4200", originPatterns = "*")
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
